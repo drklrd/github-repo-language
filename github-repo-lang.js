9d8cd871fda8a4b3d67f12eb034c6a99fb389c45
drklrd <srvbhattarai@gmail.com>
2017-05-26 20:34:52 +0545

    detect github page and appropriate user and repo. full validation not done though

--- github-repo-lang.js
+++ github-repo-lang.js
@@ -1,1 +1,12 @@
-alert(8);
+var githubUrl = window.content.location.href;
+
+if (githubUrl.split('github.com') && githubUrl.split('github.com').length && githubUrl.split('github.com')[1]) {
+	var postFixGithub = githubUrl.split('github.com')[1];
+	if (postFixGithub && postFixGithub.split('/') && postFixGithub.split('/').length && postFixGithub.split('/')[2]) {
+		var githubUser = postFixGithub.split('/')[1];
+		var currentRepo = postFixGithub.split('/')[2];
+		alert(githubUser);
+		alert(currentRepo);
+	}
+
+}
