acd5b667d31fc6688f051946ab96309432ab8127
drklrd <srvbhattarai@gmail.com>
2017-05-26 22:09:32 +0545

    add language info below repo name

--- github-repo-lang.js
+++ github-repo-lang.js
@@ -1,13 +1,28 @@
-var githubUrl = window.content.location.href;
+var langStyle = "\
+				background-color:#ffffff;\
+				border-radius:5px;\
+				color:#0366D6;\
+				padding:5px";
 
+
+var githubUrl = window.location.toString();
 if (githubUrl.split('github.com') && githubUrl.split('github.com').length && githubUrl.split('github.com')[1]) {
 	var postFixGithub = githubUrl.split('github.com')[1];
 	if (postFixGithub && postFixGithub.split('/') && postFixGithub.split('/').length && postFixGithub.split('/')[2]) {
 		var githubUser = postFixGithub.split('/')[1];
 		var currentRepo = postFixGithub.split('/')[2];
-		alert(githubUser);
-		alert(currentRepo);
-		$('.repohead-details-container').append("<br> Hello </br>")
+		$('.repohead-details-container').append("<div id='repo-lang'> <br> <br> Loading ... </br></div>");
+		$.get('https://api.github.com/users/'+githubUser+'/repos', function(userRepos) {
+			
+			for(var repo = 0; repo < userRepos.length;repo++){
+				if(userRepos[repo].name === currentRepo){
+					var activeRepoLanguage = userRepos[repo].language;
+					break;
+				}
+			}
+			if(activeRepoLanguage) $('#repo-lang').html(`<br> <br> <span style='`+langStyle+`'>  ${activeRepoLanguage} </span> `);
+		});
+		
 	}
 
 }
